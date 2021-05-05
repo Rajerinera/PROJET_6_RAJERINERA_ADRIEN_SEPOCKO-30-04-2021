76b90ed3403f60df968474cdd569c4406b7e7983
Rajerinera <adrien.rajerinera@gmail.com>
2021-05-05 12:31:24 +0200

    maskdata fixed

--- controllers/users.js
+++ controllers/users.js
@@ -2,17 +2,25 @@
 const User = require('../models/user');
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
+const maskdata = require('maskdata');
+
+const emailMask2Options = {
+    maskWith: "*", 
+    unmaskedStartCharactersBeforeAt: 3,
+    unmaskedEndCharactersAfterAt: 2,
+    maskAtTheRate: false
+};
 
 // controllers qui configure la création d'utilisateur à travers l'email 
 exports.signup = (req, res, next) =>{
     bcrypt.hash(req.body.password, 10)
     .then((hash) => {
         const user = new User({
-            email: req.body.email,
+            email: maskdata.maskEmail2(req.body.email, emailMask2Options),
             password: hash,
         });
         user.save()
-        .then(()=> res.status(201).json({messsage: "utilisateur crée!"}))
+        .then(()=> res.status(201).json({messsage: "utilisateur crée!"})) 
         .catch((error) => res.status(400).json({error}));   
     })
     .catch(error => res.status(500).json({error}));
@@ -20,7 +28,8 @@ exports.signup = (req, res, next) =>{
 
 // controllers qui configure le mot de passe des utilisateurs quand il s'autentifie avec leurs email
 exports.login = (req, res, next) => { 
-    User.findOne({email: req.body.email})
+    User.findOne({email: maskdata.maskEmail2(req.body.email, emailMask2Options),
+    })
     .then(user => {
         if(!user){
             return res.status(401).json({error: "Utilisateur non trouvé!"})
